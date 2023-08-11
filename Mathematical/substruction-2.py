2ac689a3a5091ca99ae6a58dfaa7642a510ca059
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-11 17:08:29 +0530

    #  user input method 2
    substruction-method  2

---
+++ Mathematical/substruction-2.py
@@ -0,0 +1,8 @@
+def subtraction(a, b):
+    result = a - b
+    return result
+#  user input method 2 
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+sub_result = subtraction(num1, num2)
+print("The difference is:", sub_result)
