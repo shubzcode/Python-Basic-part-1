3a889db19734c195be1447ed9c2ebf28d7bfc8ca
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-11 17:32:47 +0530

    Create find-lenght-1.py
    method 1 # method 1 already defines string
        #   finding  the length of a string.

---
+++ Strings/find-lenght-1.py
@@ -0,0 +1,8 @@
+# method 1 aleardy defines string
+    #   finding  the length of a string.
+def string_length(string):
+
+    return len(string)
+
+
+print(string_length("Shubham Sonune"))  # Prints 12
