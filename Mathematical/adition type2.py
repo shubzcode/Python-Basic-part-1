d310e15d066bc87c3755a20d244e0dc09223e069
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-11 17:04:30 +0530

    # input from user
    # input from user

---
+++ Mathematical/adition type2.py
@@ -0,0 +1,10 @@
+def addition(a, b):
+    result = a + b
+    return result
+
+# input from user
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+sum_result = addition(num1, num2)
+print("The sum is:", sum_result)
