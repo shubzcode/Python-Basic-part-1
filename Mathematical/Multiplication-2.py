82550e4884caead7a0038e6482f1bc5b6db5ec1e
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-11 17:12:34 +0530

    input from user multiplication
    input from user multiplication

---
+++ Mathematical/Multiplication-2.py
@@ -0,0 +1,8 @@
+def multiplication(a, b):
+    result = a * b
+    return result
+#  inpute from user 
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+mul_result = multiplication(num1, num2)
+print("The product is:", mul_result)
