7c7574fb34d1c52bd833a21434102173e37334cf
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-12 15:55:44 +0530

    Create sort-list-2.py

---
+++ Lists/sort-list-2.py
@@ -0,0 +1,8 @@
+# sort list another ex
+def sort_list(lst):
+    sorted_lst = sorted(lst)
+    return sorted_lst
+
+unsorted_list = [5, 2, 8, 1, 10]
+sorted_list = sort_list(unsorted_list)
+print("Sorted list:", sorted_list)
