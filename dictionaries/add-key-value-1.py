2fd331a9a9a999d97b52741924e009671e07332a
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-12 16:05:59 +0530

    set is given in dictionaries
    set is given in dictionaries method 1

---
+++ dictionaries/add-key-value-1.py
@@ -0,0 +1,8 @@
+#  set is given in dictionaries
+dict = {}
+
+dict["name"] = "John Doe"
+dict["age"] = 30
+dict["occupation"] = "Software Engineer"
+
+print(dict)  # Prints {"name": "John Doe", "age": 30, "occupation": "Software Engineer"}
