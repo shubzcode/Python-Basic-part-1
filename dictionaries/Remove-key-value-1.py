bc93798edfb3f33c8b67494d79b9af9eb487f7d6
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-12 16:09:43 +0530

    method 1
    method 1 Remove-key-value-1.py

---
+++ dictionaries/Remove-key-value-1.py
@@ -0,0 +1,5 @@
+dictionary = {"name": "John Doe", "age": 30, "occupation": "Software Engineer"}
+
+del dictionary["age"]
+
+print(dictionary)  # Prints {"age": 30, "occupation": "Software Engineer"}
