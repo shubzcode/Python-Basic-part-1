517a451b9504fd218f005bc91d82ab3ae6eaee21
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-12 23:02:18 +0530

    Update factorial-no-2.py

--- 5.Function/factorial-no-2.py
+++ 5.Function/factorial-no-2.py
@@ -4,6 +4,6 @@ def factorial(n):
     else:
         return n * factorial(n - 1)
 
-num = int(input("Enter a number: "))
+num = int(input("Enter a number: ")) # user input 
 factorial_result = factorial(num)
 print("Factorial of", num, "is:", factorial_result)
