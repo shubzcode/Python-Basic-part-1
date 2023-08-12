9fa8c1459a4bfbfab112a25acba300d006ce4b8b
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-12 23:13:46 +0530

    Create represent-a-circle-1.py

---
+++ 6.objects and classes/represent-a-circle-1.py
@@ -0,0 +1,19 @@
+class Circle:
+    """Represents a circle."""
+
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        """Calculates the area of the circle."""
+        return 3.14 * self.radius**2
+
+    def perimeter(self):
+        """Calculates the perimeter of the circle."""
+        return 2 * 3.14 * self.radius
+
+
+circle = Circle(1) #area of a c
+
+print(circle.area())
+print(circle.perimeter()) 
