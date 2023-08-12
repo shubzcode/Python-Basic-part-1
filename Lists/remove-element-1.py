58cd076420151c2c7c05f4adc9b8a25ac7916835
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-12 15:48:19 +0530

    Update remove-element-1.py
    adding comment

--- Lists/remove-element-1.py
+++ Lists/remove-element-1.py
@@ -1,6 +1,6 @@
 # Removing elements from a list:
 list = [1, 2, 3, 4, 5]
 # selected element to remove
-list.remove(3)
+list.remove(3) # selected element to remove
 
 print(list)  # Prints [1, 2, 4, 5]
