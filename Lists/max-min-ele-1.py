362b0115cf059fda44fade98bf2d2b27e5907433
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-12 15:58:21 +0530

    method 1 already given list
    already given

---
+++ Lists/max-min-ele-1.py
@@ -0,0 +1,9 @@
+# Finding Maximum and Minimum Values in a List:
+
+list = [1, 5, 3, 2, 4]
+
+maximum_value = max(list)
+minimum_value = min(list)
+
+print(maximum_value)  # Prints 5
+print(minimum_value)  # Prints 1
