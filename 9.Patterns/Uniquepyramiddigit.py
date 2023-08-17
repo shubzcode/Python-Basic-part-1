9408983e6cd1a96ec94d4ed778ace7f34b7beb5c
SHUBHAM SONUNE <shubham8262@gmail.com>
2023-08-17 12:03:55 +0530

    Update Uniquepyramiddigit.py
    update code

--- 9.Patterns/Uniquepyramiddigit.py
+++ 9.Patterns/Uniquepyramiddigit.py
@@ -1,7 +1,7 @@
 rows = 6
-for i in range(1,rows + 1):
-    for j in range (1, i - 1):
+for i in range(1, rows + 1):
+    for j in range(1, i):
         print(j, end=" ")
-    for j in range (i - 1, 0, -1):
+    for j in range(i - 1, 0, -1):
         print(j, end=" ")
-        print()
+    print()
